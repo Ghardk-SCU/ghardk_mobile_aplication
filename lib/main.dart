a9c43d53eff1c6bfbbc0d1e57e9b617c08336939
MahmoudElsayedJrr <mr.houda2001@gmail.com>
2024-02-12 22:49:54 +0200

    add nunitoSans font to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:final_project/core/utilits/constant.dart';
 import 'package:final_project/presentation/mainPage/mainPage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme:
-          ThemeData.light().copyWith(scaffoldBackgroundColor: kScaffoldColor),
+      theme: ThemeData.light().copyWith(
+          scaffoldBackgroundColor: kScaffoldColor,
+          textTheme:
+              GoogleFonts.nunitoSansTextTheme(Theme.of(context).textTheme)),
       debugShowCheckedModeBanner: false,
       home: mainPage(),
     );
